f0bbda72f08cb9506d4d54771b845763c49a0e07
Will <wruann8@gmail.com>
2022-07-07 09:49:27 -0400

    .
    .

---
+++ Automation/cursor.py
@@ -0,0 +1,21 @@
+import pyautogui
+import math
+import time
+
+# Radius 
+R = 400
+# measuring screen size
+(x,y) = pyautogui.size()
+# locating center of the screen 
+(X,Y) = pyautogui.position(x/2,y/2)
+# offsetting by radius 
+pyautogui.moveTo(X+R,Y)
+
+
+running = True 
+while running: 
+    time.sleep(1)
+    for i in range(360):
+        # setting pace with a modulus 
+        if i%6==0:
+            pyautogui.moveTo(X+R*math.cos(math.radians(i)),Y+R*math.sin(math.radians(i)), duration = 1)
